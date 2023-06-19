8e9e97276d44804c71e61bea448f29e193dcd06f
con1759 <con1759@anarock.com>
2023-06-19 12:18:59 +0530

    Added Navigation Menu

--- app/src/main/java/com/example/sharingdatabetweentwofragment/MainActivity.kt
+++ app/src/main/java/com/example/sharingdatabetweentwofragment/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.sharingdatabetweentwofragment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+        val bottomnav = findViewById<BottomNavigationView>(R.id.navigationmenu)
+         bottomnav.setupWithNavController(navController)
+
+
+
+
+
     }
 }
